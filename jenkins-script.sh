78b17a0ae3d13578d0f73bf09032a360c8c6e467
Gauri Kulkarni <142674318+Gaurii-01@users.noreply.github.com>
2025-08-08 19:08:43 +0530

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-# Path to MOTD file in your repo
-MOTD_FILE="motd.txt"
-
-# Write new MOTD content
-echo "Hello from Jenkins - $(date)" > "$MOTD_FILE"
-
-# Update system MOTD (optional: needs sudo privileges on Jenkins agent)
-# echo "Hello from Jenkins - $(date)" | sudo tee /etc/motd > /dev/null
+echo "Hello From Jenkins" | sudo tee /etc/motd > /dev/null
